1482cc3082fdd5911060bc45a6815d3813a10520
Nathelin <menachonatalian@gmail.com>
2025-11-09 00:31:33 -0300

    Se crearon los archivos base para el Escenario 1 y 3

---
+++ src/modelos/escenario1/Escenario1.java
@@ -0,0 +1,8 @@
+package modelos.escenario1;
+
+public class Escenario1 {
+    /* si querés desglosalo en más archivos. estoy tomando de referencia que los profes suelen separar en carpetas 
+    *  por cada uno de los ejemplos que hacen
+    *  si preferis que hagamos de otra forma cambiale nomas wi
+    */ 
+}
